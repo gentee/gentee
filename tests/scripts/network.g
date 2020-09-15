7623805229afaae5245ac39e166ff55440474cbf
gentee <akrivonogov@gmail.com>
2020-09-15 17:20:10 +0500

    Added HTTPRequest func

--- tests/scripts/network.g
+++ tests/scripts/network.g
@@ -1,9 +1,22 @@
 const {
    README = `https://github.com/gentee/gentee`
    TITLE = `Gentee programming language`
+   TESTURL = `https://..../`
+}
+
+func testrequest() {
+    map empty head
+    str response = HTTPRequest(TESTURL + "get", "GET", empty,head)
+    Println(response)
+    map params = {  `par1`: `Имя`, `value_2`: `This is a string`  }
+    Println(HTTPRequest(TESTURL + "get", "GET", params, head))
+    Println(HTTPRequest(TESTURL + "post", "POST", params, head))
+    map headjson = { `Content-Type`: `application/json; charset=UTF-8` }
+    Println(HTTPRequest(TESTURL + "post", "POST", params, headjson))
 }
 
 run str {
+//    testrequest()
     str page = HTTPPage(README)
     if Find(page, TITLE) < 0 : error(100, `HTTPPage`)
     buf bufPage = HTTPGet(README)
--- vm/network.go
+++ vm/network.go
@@ -5,10 +5,14 @@
 package vm
 
 import (
+	"bytes"
+	"encoding/json"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
 	"github.com/gentee/gentee/core"
 )
@@ -55,4 +59,68 @@ func HTTPPage(url string) (string, error) {
 		}
 	}
 	return ret, err
+}
+
+// HTTPRequest send HTTP request to the specified URL and returns a string result.
+func HTTPRequest(urlPath string, method string, params *core.Map, headers *core.Map) (ret string,
+	err error) {
+	var (
+		req         *http.Request
+		buf         []byte
+		contentType string
+		isForm      bool
+		body        io.Reader
+	)
+	for _, key := range headers.Keys {
+		if key == `Content-Type` {
+			contentType = headers.Data[key].(string)
+			break
+		}
+	}
+	if method != `GET` && len(params.Data) > 0 {
+		if strings.HasPrefix(contentType, `application/json`) {
+			var jsonBody []byte
+			jsonBody, err = json.Marshal(params.Data)
+			if err != nil {
+				return
+			}
+			body = bytes.NewBuffer(jsonBody)
+		} else {
+			parVals := url.Values{}
+			for _, key := range params.Keys {
+				parVals.Add(key, params.Data[key].(string))
+			}
+			body = strings.NewReader(parVals.Encode())
+			isForm = len(contentType) == 0
+		}
+	}
+	if req, err = http.NewRequest(method, urlPath, body); err != nil {
+		return
+	}
+
+	if method == `GET` && len(params.Data) > 0 {
+		q := req.URL.Query()
+		for _, key := range params.Keys {
+			q.Add(key, params.Data[key].(string))
+		}
+		req.URL.RawQuery = q.Encode()
+	}
+
+	for _, key := range headers.Keys {
+		req.Header.Add(key, headers.Data[key].(string))
+	}
+	//	req.Header.Add("Content-Type", "application/json; charset=UTF-8")
+	if isForm {
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err == nil {
+		buf, err = ioutil.ReadAll(res.Body)
+		res.Body.Close()
+		if err == nil {
+			ret = string(buf)
+		}
+	}
+	return
 }
