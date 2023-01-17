ec16d692eb3964aec80fa17ee59abc66ba1a8fad
a <a@tuxpa.in>
2023-01-16 23:24:58 -0600

    fix

--- cli/gentee.go
+++ cli/gentee.go
@@ -9,38 +9,20 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"path/filepath"
 	"strings"
 
 	gentee "github.com/gentee/gentee"
-	"github.com/gentee/gentee/vm"
 )
 
-const (
-	errNoFile = iota + 1
-	errCompile
-	errRun
-	errResult
-	errPlaceholder5
-	errPlaceholder6
-	errPlaceholder7
-	errUndefined
-)
-
-type CodedError struct {
-	Code int
-	Err  error
+type Cli struct {
+	workspace *gentee.Gentee
+	args      CommandArgs
 }
 
-func (c *CodedError) Error() string {
-	if c.Err != nil {
-		return c.Error()
-	}
-	return fmt.Sprintf("Unspecified Error with Code: %d", c.Code)
-}
-
-func (c *CodedError) Unwrap() error {
-	return c.Err
+func (c *Cli) Init() *Cli {
+	c.workspace = gentee.New()
+	c.args.Parse()
+	return c
 }
 
 type CommandArgs struct {
@@ -64,15 +46,41 @@ func (c *CommandArgs) Parse() *CommandArgs {
 	return c
 }
 
-type Cli struct {
-	workspace *gentee.Gentee
-	args      CommandArgs
+func (c *Cli) Exec() {
+	stderr := os.Stderr
+	err := c.exec()
+	if err != nil {
+		if coded, ok := err.(*CodedError); ok {
+			fmt.Fprintf(stderr, "%s", coded.Unwrap())
+			os.Exit(coded.Code)
+		}
+		fmt.Fprintf(stderr, "%s", err)
+		os.Exit(errUndefined)
+	}
+	os.Exit(0)
 }
 
-func (c *Cli) Init() *Cli {
-	c.workspace = gentee.New()
-	c.args.Parse()
-	return c
+func (c *Cli) exec() error {
+	w := os.Stdout
+	if c.args.Output != "" {
+		fp, err := os.OpenFile(c.args.Output, os.O_TRUNC|os.O_CREATE|os.O_RDWR, 0o640)
+		if err != nil {
+			return err
+		}
+		defer fp.Close()
+		w = fp
+	}
+	switch {
+	case c.args.Ver:
+		c.exec_Ver()
+	default:
+		return c.exec_RunFile(w)
+	}
+	return nil
+}
+
+func (c *Cli) exec_Ver() {
+	fmt.Println(gentee.Version())
 }
 
 func (c *Cli) exec_RunFile(w io.Writer) error {
@@ -109,47 +117,10 @@ func (c *Cli) exec_RunFile(w io.Writer) error {
 		if len(ret) > 0 && ret == strings.TrimSpace(resultStr) {
 			return nil
 		}
-		err = fmt.Errorf(`different test result %s`, resultStr)
-		return codedError(err, errResult)
+		return codedError(fmt.Errorf(`different test result %s`, resultStr), errResult)
 	}
 	if result != nil {
 		fmt.Fprintf(w, resultStr)
-	}
-	return nil
-}
-
-func (c *Cli) exec_Ver() {
-	fmt.Println(gentee.Version())
-}
-
-func (c *Cli) Exec() {
-	stderr := os.Stderr
-	err := c.exec()
-	if err != nil {
-		if coded, ok := err.(*CodedError); ok {
-			fmt.Fprintf(stderr, "%s", coded.Unwrap())
-			os.Exit(coded.Code)
-		}
-		fmt.Fprintf(stderr, "%s", err)
-		os.Exit(errUndefined)
-	}
-	os.Exit(0)
-}
-func (c *Cli) exec() error {
-	w := os.Stdout
-	if c.args.Output != "" {
-		fp, err := os.OpenFile(c.args.Output, os.O_TRUNC|os.O_CREATE|os.O_RDWR, 0o640)
-		if err != nil {
-			return err
-		}
-		defer fp.Close()
-		w = fp
-	}
-	switch {
-	case c.args.Ver:
-		c.exec_Ver()
-	default:
-		return c.exec_RunFile(w)
 	}
 	return nil
 }
@@ -159,26 +130,5 @@ func main() {
 	if cli.args.Ver {
 		return
 	}
-	cli.exec()
-}
-func codedError(err error, code int) error {
-	if err != nil {
-		fmt.Print(`ERROR`)
-		if errTrace, ok := err.(*vm.RuntimeError); ok {
-			fmt.Printf(" #%d: %s\n", errTrace.ID, err.Error())
-			for _, trace := range errTrace.Trace {
-				path := trace.Path
-				dirs := strings.Split(filepath.ToSlash(path), `/`)
-				if len(dirs) > 3 {
-					path = `...` + path[len(path)-len(strings.Join(dirs[len(dirs)-3:], `/`))-1:]
-				}
-				fmt.Printf("%s [%d:%d] %s -> %s\n", path, trace.Line, trace.Pos, trace.Entry, trace.Func)
-			}
-			code = errTrace.ID
-		} else {
-			fmt.Println(`:`, err.Error())
-		}
-		return &CodedError{Err: err, Code: code}
-	}
-	return nil
+	cli.Exec()
 }
